c664d40cf0c90f5508c9b952903d6e191bcbc072
Nataleesha <frojlan@gmail.com>
2023-07-09 13:05:16 +0300

    secrets

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://exercisedb.p.rapidapi.com";
-axios.defaults.headers.common["X-RapidAPI-Key"] =
-  "45fe0b6de0mshc9d34cfde72bf5dp1b80d7jsn5c098af3d35c";
+axios.defaults.headers.common["X-RapidAPI-Key"] = import.meta.env.VITE_API_KEY;
 axios.defaults.headers.common["X-RapidAPI-Host"] = "exercisedb.p.rapidapi.com";
 
 export const fetchData = async (url) => {
